86f8f39d8042747b02fcbc5e6e781f3e8b408f2f
Basantgrg924 <basantgrg007@gmail.com>
2025-08-21 21:06:27 +0545

    updated server.js to work with new routes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,20 @@
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
+const queueRoutes = require('./routes/queueRoutes');
 dotenv.config();
-
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-//app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/queues', queueRoutes);
 
 // Export the app object for testing
 if (require.main === module) {
-    connectDB();
-    // If the file is run directly, start the server
-    const PORT = process.env.PORT || 5001;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  }
-
+  connectDB();
+  const PORT = process.env.PORT || 5001;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 module.exports = app
